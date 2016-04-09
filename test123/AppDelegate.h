7c2745d035ae68ba0017e655c24d7d891dbb4c14
杜甲 <815319775@qq.com>
2016-04-09 09:18:12 +0800

    commit

---
+++ test123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test123
+//
+//  Created by 杜甲 on 16/4/9.
+//  Copyright © 2016年 杜甲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test123
+//
+//  Created by 杜甲 on 16/4/9.
+//  Copyright © 2016年 杜甲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
